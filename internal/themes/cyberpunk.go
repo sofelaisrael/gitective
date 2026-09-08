5df80e395c2245685d42353a890f71ed52bf151e
kenshin <129384534+sofelaisrael@users.noreply.github.com>
2026-09-07 17:31:07 -0700

    feat: theme personality

--- internal/themes/cyberpunk.go
+++ internal/themes/cyberpunk.go
@@ -2,7 +2,9 @@ package themes
 
 import (
 	"fmt"
+
 	"github.com/sofelaisrael/gitective/internal/commit"
+	"github.com/sofelaisrael/gitective/internal/personality"
 )
 
 type CyberpunkTheme struct{}
@@ -10,5 +12,6 @@ type CyberpunkTheme struct{}
 func (CyberpunkTheme) Name() string { return "cyberpunk" }
 
 func (CyberpunkTheme) Render(facts commit.CommitFacts) string {
-	return fmt.Sprintf("⚡ NIGHT//PROTOCOL\n\nTarget: %s\nAuthor: %s\nFiles: %d  +%d/-%d\nFiles list: %v\nStatus: SYSTEM SECURED.", facts.Message, facts.Author, facts.FilesChanged, facts.LinesAdded, facts.LinesDeleted, facts.Files)
+	p := personality.Classify(facts)
+	return fmt.Sprintf("⚡ NIGHT//PROTOCOL\n\nTarget: %s\nAuthor: %s\nFiles: %d  +%d/-%d\nFiles list: %v\nPersonality: %s\nStatus: SYSTEM SECURED.", facts.Message, facts.Author, facts.FilesChanged, facts.LinesAdded, facts.LinesDeleted, facts.Files, p)
 }
--- internal/themes/renaissance.go
+++ internal/themes/renaissance.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/sofelaisrael/gitective/internal/commit"
+	"github.com/sofelaisrael/gitective/internal/personality"
 )
 
 type RenaissanceTheme struct{}
@@ -11,5 +12,5 @@ type RenaissanceTheme struct{}
 func (RenaissanceTheme) Name() string { return "renaissance" }
 
 func (RenaissanceTheme) Render(facts commit.CommitFacts) string {
-	return fmt.Sprintf("⚜ THE CHRONICLES\n\nUpon this day, the keeper %s hath wrought '%s'.\nScrolls altered: %d\nLines summoned: %d\nLines cast away: %d\nThus the realm endureth.", facts.Author, facts.Message, facts.FilesChanged, facts.LinesAdded, facts.LinesDeleted)
+	return fmt.Sprintf("⚜ THE CHRONICLES\n\nUpon this day, the keeper %s hath wrought '%s'.\nScrolls altered: %d\nLines summoned: %d\nLines cast away: %d\nCharacter: %s\nThus the realm endureth.", facts.Author, facts.Message, facts.FilesChanged, facts.LinesAdded, facts.LinesDeleted, personality.Classify(facts))
 }
