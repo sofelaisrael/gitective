36db5b6bd5fb623627fb91eb7a7f482123c7b27f
kenshin <sofelaisrael@users.noreply.github.com>
2026-09-20 02:44:46 +0100

    chore: remove dead analysis package, upgrade themes to lipgloss

--- internal/themes/cyberpunk.go
+++ internal/themes/cyberpunk.go
@@ -3,8 +3,22 @@ package themes
 import (
 	"fmt"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/sofelaisrael/gitective/internal/commit"
 	"github.com/sofelaisrael/gitective/internal/personality"
+)
+
+var (
+	accentColor = lipgloss.Color("#FF3366")
+	accentStyle = lipgloss.NewStyle().Foreground(accentColor).Bold(true)
+	dimStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("#666666"))
+	labelStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#888888"))
+	valueStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFFFFF"))
+	boxStyle    = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("#FF3366")).
+			PaddingLeft(1).PaddingRight(1).
+			MarginTop(1)
 )
 
 type CyberpunkTheme struct{}
@@ -12,16 +26,59 @@ type CyberpunkTheme struct{}
 func (CyberpunkTheme) Name() string { return "cyberpunk" }
 
 func (CyberpunkTheme) Render(facts commit.CommitFacts) string {
+	var tag, detail string
+
 	switch personality.Classify(facts) {
 	case personality.PanicPatch:
-		return fmt.Sprintf("// EMERGENCY PATCH\nOperator: %s\nBreach: %s\nSectors: %d  +%d/-%d\nAction: hot-patched at %02d:00\nStatus: CONTAINED.", facts.Author, facts.Message, facts.FilesChanged, facts.LinesAdded, facts.LinesDeleted, facts.Timestamp.Hour())
+		tag = "EMERGENCY PATCH"
+		detail = fmt.Sprintf("hot-patched at %02d:00", facts.Timestamp.Hour())
 	case personality.NuclearJanitor:
-		return fmt.Sprintf("// PURGE PROTOCOL\nOperator: %s\nErased: %d lines from %d sectors\nPayload: %s\nStatus: GRID CLEAN. No witnesses.", facts.Author, facts.LinesDeleted, facts.FilesChanged, facts.Message)
+		tag = "PURGE PROTOCOL"
+		detail = fmt.Sprintf("erased %d lines from %d sectors", facts.LinesDeleted, facts.FilesChanged)
 	case personality.MadScientist:
-		return fmt.Sprintf("// NEO-FORGE\nOperator: %s\nForged: %d sectors + %d lines\nConstruct: %s\nStatus: UNSTABLE. Proceed.", facts.Author, facts.FilesChanged, facts.LinesAdded, facts.Message)
+		tag = "NEO-FORGE"
+		detail = fmt.Sprintf("forged %d sectors + %d lines", facts.FilesChanged, facts.LinesAdded)
 	case personality.SilentCommit:
-		return fmt.Sprintf("// GHOST SIGNAL\nOperator: %s\nTransmission: \"%s\"\nSectors: %d\nStatus: NO PAYLOAD. Ghosted.", facts.Author, facts.Message, facts.FilesChanged)
+		tag = "GHOST SIGNAL"
+		detail = "no payload"
 	default:
-		return fmt.Sprintf("// FREIGHT RUN\nOperator: %s\nPayload: %s\nSectors: %d  +%d/-%d\nStatus: DELIVERED.", facts.Author, facts.Message, facts.FilesChanged, facts.LinesAdded, facts.LinesDeleted)
+		tag = "FREIGHT RUN"
+		detail = fmt.Sprintf("%d sectors  +%d/-%d", facts.FilesChanged, facts.LinesAdded, facts.LinesDeleted)
+	}
+
+	header := accentStyle.Render(fmt.Sprintf("// %s", tag))
+
+	left := fmt.Sprintf("%s  %s\n%s  %s\n%s  %s",
+		labelStyle.Render("Operator"), valueStyle.Render(facts.Author),
+		labelStyle.Render("Target"), valueStyle.Render(truncate(facts.Message, 40)),
+		labelStyle.Render("Detail"), dimStyle.Render(detail),
+	)
+
+	left = lipgloss.NewStyle().MarginLeft(0).Render(left)
+
+	bottom := accentStyle.Render("■ ") + dimStyle.Render("STATUS: ") + valueStyle.Render(statusWord(personality.Classify(facts)))
+
+	return boxStyle.Render(fmt.Sprintf("%s\n\n%s\n\n%s", header, left, bottom))
+}
+
+func truncate(s string, max int) string {
+	if len(s) <= max {
+		return s
+	}
+	return s[:max-3] + "..."
+}
+
+func statusWord(p personality.Personality) string {
+	switch p {
+	case personality.PanicPatch:
+		return "CONTAINED"
+	case personality.NuclearJanitor:
+		return "GRID CLEAN"
+	case personality.MadScientist:
+		return "UNSTABLE"
+	case personality.SilentCommit:
+		return "GHOSTED"
+	default:
+		return "DELIVERED"
 	}
 }
--- internal/themes/renaissance.go
+++ internal/themes/renaissance.go
@@ -3,8 +3,22 @@ package themes
 import (
 	"fmt"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/sofelaisrael/gitective/internal/commit"
 	"github.com/sofelaisrael/gitective/internal/personality"
+)
+
+var (
+	renaColor  = lipgloss.Color("#C9A96E")
+	renaAccent = lipgloss.NewStyle().Foreground(renaColor).Bold(true)
+	renaDim    = lipgloss.NewStyle().Foreground(lipgloss.Color("#888888"))
+	renaLabel  = lipgloss.NewStyle().Foreground(lipgloss.Color("#AAAAAA"))
+	renaValue  = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFFFFF"))
+	renaBox    = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(renaColor).
+			PaddingLeft(1).PaddingRight(1).
+			MarginTop(1)
 )
 
 type RenaissanceTheme struct{}
@@ -12,23 +26,34 @@ type RenaissanceTheme struct{}
 func (RenaissanceTheme) Name() string { return "renaissance" }
 
 func (RenaissanceTheme) Render(facts commit.CommitFacts) string {
-	p := personality.Classify(facts)
-	var verseName string
-	switch p {
+	var tag string
+
+	switch personality.Classify(facts) {
 	case personality.PanicPatch:
-		verseName = "THE HASTY MENDER"
+		tag = "THE HASTY MENDER"
 	case personality.NuclearJanitor:
-		verseName = "THE GRAND PURGATION"
+		tag = "THE GRAND PURGATION"
 	case personality.Janitor:
-		verseName = "THE SCRIBE OF DUST"
+		tag = "THE SCRIBE OF DUST"
 	case personality.MadScientist:
-		verseName = "THE MASTER ARTIFICER"
+		tag = "THE MASTER ARTIFICER"
 	case personality.Architect:
-		verseName = "THE CATHEDRAL BUILDER"
+		tag = "THE CATHEDRAL BUILDER"
 	case personality.SilentCommit:
-		verseName = "THE SILENT VOW"
-	case personality.Shipper:
-		verseName = "THE CARAVAN"
+		tag = "THE SILENT VOW"
+	default:
+		tag = "THE CARAVAN"
 	}
-	return fmt.Sprintf("⚜ THE CHRONICLES [%s]\n\nUpon this day, keeper %s hath wrought '%s'.\nScrolls: %d  +%d/-%d\nThus endureth.", verseName, facts.Author, facts.Message, facts.FilesChanged, facts.LinesAdded, facts.LinesDeleted)
+
+	header := renaAccent.Render(fmt.Sprintf("⚜ %s", tag))
+
+	body := fmt.Sprintf("%s  %s\n%s  '%s'\n%s  %d scrolls  +%d/-%d",
+		renaLabel.Render("Keeper"), renaValue.Render(facts.Author),
+		renaLabel.Render("Wrought"), renaValue.Render(facts.Message),
+		renaLabel.Render("Scribe"), facts.FilesChanged, facts.LinesAdded, facts.LinesDeleted,
+	)
+
+	bottom := renaAccent.Render("■ ") + renaDim.Render("Thus endureth.")
+
+	return renaBox.Render(fmt.Sprintf("%s\n\n%s\n\n%s", header, body, bottom))
 }
